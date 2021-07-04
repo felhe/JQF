8a590ed1ae1f0931715f182a21d86fbc94ae16bb
Stephan Druskat <mail@sdruskat.net>
2021-07-04 21:09:04 +0200

    Override console instance and method returning banner
    Co-authored-by: felhe <10731299+felhe@users.noreply.github.com>
    Co-authored-by: Peter Wegmann <wegmannp@hu-berlin.de>

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/ei/PestGuidance.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/ei/PestGuidance.java
@@ -5,11 +5,16 @@ package edu.berkeley.cs.jqf.fuzz.ei;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.time.Duration;
 
 /**
+ * A guidance that performs coverage-guided fuzzing using two coverage maps,
+ * one for all inputs and one for valid inputs only, that perform well.
+ * 
+ * @author Felix Leonard Heitmann
  * @author Stephan Druskat
- *
+ * @author Peter Wegmann
  */
 public class PestGuidance extends ZestGuidance {
 	
@@ -19,7 +24,8 @@ public class PestGuidance extends ZestGuidance {
 	/** Multiplication factor for number of children to produce for favored inputs. */
 	protected final int NUM_CHILDREN_MULTIPLIER_FAVORED = 50;
 
-
+	/** Overriding the console used in ZestGuidance, TODO Check if this can be removed. */
+    protected final PrintStream console = System.out;
 
 	/**
 	 * @param testName
@@ -29,7 +35,6 @@ public class PestGuidance extends ZestGuidance {
 	 */
 	public PestGuidance(String testName, Duration duration, File outputDirectory) throws IOException {
 		super(testName, duration, outputDirectory);
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -42,7 +47,6 @@ public class PestGuidance extends ZestGuidance {
 	public PestGuidance(String testName, Duration duration, File outputDirectory, File[] seedInputFiles)
 			throws IOException {
 		super(testName, duration, outputDirectory, seedInputFiles);
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -55,7 +59,19 @@ public class PestGuidance extends ZestGuidance {
 	public PestGuidance(String testName, Duration duration, File outputDirectory, File seedInputDir)
 			throws IOException {
 		super(testName, duration, outputDirectory, seedInputDir);
-		// TODO Auto-generated constructor stub
 	}
+	
+    /* Returns the banner to be displayed on the status screen */
+    protected String getTitle() {
+        if (blind) {
+            return  "Generator-based random fuzzing (no guidance)\n" +
+                    "--------------------------------------------\n";
+        } else {
+            return  "Semantic Fuzzing with Pest\n" +
+            		"(Pest is Performant Zest.)\n" +
+                    "--------------------------\n";
+        }
+    }
+
 
 }
